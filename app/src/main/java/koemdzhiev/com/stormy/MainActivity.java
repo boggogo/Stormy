31460ec412bdd9d93b6262eec93d0b12225d4dd5
boggogo <koemdjiev@gmail.com>
2015-05-16 00:49:00 +0100

    - currentWeather class and functionality added. - populating a CurrentWeather object within the onResponse method with the jsonData, using JSONObject's methods

--- app/src/main/java/koemdzhiev/com/stormy/MainActivity.java
+++ app/src/main/java/koemdzhiev/com/stormy/MainActivity.java
@@ -18,18 +18,25 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 
 public class MainActivity extends Activity {
     public static final String TAG = MainActivity.class.getSimpleName();
+    private CurrentWeather mCurrentWeather;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //-----------MY CODE STARTS HERE------------------
         String API_KEY = "3ed3a1906736c6f6c467606bd1f91e2c";
-        double latitude = 37.8267;
-        double longtitude = -122.423;
+        //aberdeen, coordinates
+        double latitude = 57.1531;
+        double longtitude = -2.0928;
         String forecast = "https://api.forecast.io/forecast/"+ API_KEY +"/"+ latitude+","+ longtitude;
 
         if(isNetworkAvailable()) {
@@ -37,6 +44,7 @@ public class MainActivity extends Activity {
             Request request = new Request.Builder()
                     .url(forecast)
                     .build();
+
             Call call = client.newCall(request);
 
             call.enqueue(new Callback() {
@@ -44,17 +52,22 @@ public class MainActivity extends Activity {
                 public void onFailure(Request request, IOException e) {
 
                 }
-
+                //when the call to the Okhttp library finishes, than calls this method:
                 @Override
                 public void onResponse(Response response) throws IOException {
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        //Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-
+                            mCurrentWeather = getCurrentDetails(jsonData);
+                            //do sth with the forecast details
                         } else {
                             alertUserAboutError();
                         }
                     } catch (IOException e) {
+                        Log.e(TAG, "Exception caught:", e);
+                    }
+                    catch (JSONException e){
                         Log.e(TAG, "Exception caught:", e);
                     }
                 }
@@ -62,11 +75,31 @@ public class MainActivity extends Activity {
         }else{
             //Toast.makeText(this,getString(R.string.network_unavailable_message),Toast.LENGTH_LONG).show();
             WIFIDialogFragment dialog = new WIFIDialogFragment();
-            dialog.show(getFragmentManager(),getString(R.string.error_dialog_text));
+            dialog.show(getFragmentManager(), getString(R.string.error_dialog_text));
         }
         Log.d(TAG, "Main UI code is running!");
 
 
+    }
+    //throws JSONException, doing it like that, we place the
+    // responsability of handaling this exeption to the caller of the method
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG,"From JSON: " + timezone);
+
+        JSONObject currently = forecast.getJSONObject("currently");
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setPrecipChange(currently.getDouble("precipProbability"));
+        currentWeather.setSummery(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timezone);
+
+        Log.d(TAG,currentWeather.getFormattedTime());
+        return currentWeather;
     }
 
     private boolean isNetworkAvailable() {
