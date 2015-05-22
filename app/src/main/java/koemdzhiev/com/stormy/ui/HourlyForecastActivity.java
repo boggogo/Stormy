1ae2d4e5a6f2d9319c11fee17a0485f434e6b8fb
boggogo <koemdjiev@gmail.com>
2015-05-22 23:14:50 +0100

    -added RecyclerView -included Hourly button functionality

--- app/src/main/java/koemdzhiev/com/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/koemdzhiev/com/stormy/ui/HourlyForecastActivity.java
@@ -1,26 +1,43 @@
 package koemdzhiev.com.stormy.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.Arrays;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import koemdzhiev.com.stormy.R;
+import koemdzhiev.com.stormy.adapters.HourAdapter;
 import koemdzhiev.com.stormy.weather.Hour;
 
-public class HourlyForecastActivity extends ActionBarActivity {
+public class HourlyForecastActivity extends Activity {
 
     private Hour[] mHours;
+    //inject the RecyclerView as member variable
+    @InjectView(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
 
+        ButterKnife.inject(this);
+
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables,parcelables.length,Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        //if dealing with fixed size data, it is recommended to do the following...
+        mRecyclerView.setHasFixedSize(true);
     }
 }
